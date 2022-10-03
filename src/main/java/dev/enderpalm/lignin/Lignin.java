3fd57b066714a9d36d41832cf4781bfde42b71cf
Enderpalm <ch.enderpalm@gmail.com>
2022-10-03 16:27:27 +0700

    Initial Implementation

---
+++ src/main/java/dev/enderpalm/lignin/Lignin.java
@@ -0,0 +1,11 @@
+package dev.enderpalm.lignin;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Lignin implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
