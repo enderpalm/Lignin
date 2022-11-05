dfdf4e64715bbed26ec8866d6d595bd866eee0e8
Enderpalm <ch.enderpalm@gmail.com>
2022-11-05 10:49:17 +0700

    chore: create `parchment_version` in gradle.properties

--- src/main/java/dev/enderpalm/lignin/client/LigninClient.java
+++ src/main/java/dev/enderpalm/lignin/client/LigninClient.java
@@ -1,10 +1,7 @@
 package dev.enderpalm.lignin.client;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
 
-@Environment(EnvType.CLIENT)
 public class LigninClient implements ClientModInitializer {
 
     @Override
