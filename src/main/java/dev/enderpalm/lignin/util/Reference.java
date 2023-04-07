fc5cb06d9563d0c3f4c5b3f80ae1808e893ed9bc
Enderpalm CI <ch.enderpalm@gmail.com>
2023-04-07 14:44:00 +0700

    Mock implementation inner mod (#5)
    * code: add mock implementation mod in `mock` package
    
    * docs(Reference.java): add javadoc to constructor, `ResourceLocation`, env checking
    
    [skip ci]

--- src/main/java/dev/enderpalm/lignin/util/Reference.java
+++ src/main/java/dev/enderpalm/lignin/util/Reference.java
@@ -16,6 +16,10 @@ public final class Reference {
     private final Collection<Person> contributors;
     private final String modVersion;
 
+    /**
+     * Creates a new {@link Reference} instance, using provided mod ID to get metadata
+     * @param modID the mod ID
+     */
     public Reference(String modID){
         this.modID = modID;
         ModMetadata metadata = FabricLoader.getInstance().getModContainer(this.modID).orElseThrow().getMetadata();
@@ -44,17 +48,40 @@ public final class Reference {
         return Minecraft.getInstance().getLaunchedVersion();
     }
 
+    /**
+     * Checks if the mock implementation of LigninLib is <u>disabled</u>
+     * @return <b>true</b> if <code>-Dlignin.disableMock</code> is set to <code>true</code>
+     * or if the environment is not a development environment
+     * @see #isDevEnv()
+     */
+    public static boolean isMockDisabled(){
+        return Boolean.getBoolean("lignin.disableMock") || !isDevEnv();
+    }
+
+    /**
+     * Checks if the environment is a dev environment using Fabric Loader
+     * @return <b>true</b> if check passes
+     */
     public static boolean isDevEnv(){
         return FabricLoader.getInstance().isDevelopmentEnvironment();
     }
 
+    /**
+     * Creates a new {@link ResourceLocation} with the mod ID as the namespace
+     * @param id the path of the resource
+     * @return the new {@link ResourceLocation}
+     */
     public @NotNull ResourceLocation path(String id){
         return new ResourceLocation(this.modID, id);
     }
 
+    /**
+     * Creates a new {@link ResourceLocation} with the mod ID as the namespace
+     * @param id the path of the resource
+     * @return String representation of the {@link ResourceLocation}
+     */
     public @NotNull String pathLiteral(String id){
         return String.format("%s:%s",this.modID,id);
     }
-
 
 }
